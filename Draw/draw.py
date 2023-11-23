d82a4771f904c96f07685354c122989ac44b305f
Cynthia <cynthiaiii4@hotmail.com>
2023-11-23 11:51:54 +0800

    facegrid子圖換成回歸圖

--- Draw/draw.py
+++ Draw/draw.py
@@ -63,7 +63,7 @@ def Draw(data,features,*xml):
             sns.scatterplot(data=data, x=values['X'][0], y=y)
             plt.xticks(rotation=90)
             plt.title(f'Scatter Plot of {values["X"][0]} vs {y}')
-            plt.savefig('graph/scatter.png')
+            plt.savefig('graph/4/scatter.png')
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -73,7 +73,7 @@ def Draw(data,features,*xml):
             sns.catplot(x=values['X'][0], y=y, data=data, kind= chart_parm )  
             plt.xticks(rotation=90)
             plt.title(f'Catplot of {values["X"][0]} vs {y}')
-            plt.savefig('graph/catplot.png')
+            plt.savefig('graph/4/catplot.png')
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -82,7 +82,7 @@ def Draw(data,features,*xml):
 
             fig = plt.figure()
             sns.pairplot(data, vars=values['X'])
-            plt.savefig('graph/pairplot.png')
+            plt.savefig('graph/4/pairplot.png')
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -92,7 +92,7 @@ def Draw(data,features,*xml):
             fig = plt.figure()
             ax = sns.countplot(x=values['X'][0], data=data) 
             plt.xticks(rotation=90)
-            plt.savefig('graph/countplot.png')
+            plt.savefig('graph/4/countplot.png')
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -102,7 +102,7 @@ def Draw(data,features,*xml):
             fig = plt.figure()
             sns.displot(data=data[values['X'][0]], kde=True, bins=20)
             plt.xticks(rotation=90)
-            plt.savefig('graph/displot.png') 
+            plt.savefig('graph/4/displot.png') 
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -111,7 +111,7 @@ def Draw(data,features,*xml):
             fig = plt.figure()
             ax = sns.regplot(x=values['X'][0], y=y, data=data)
             plt.xticks(rotation=90)
-            plt.savefig('graph/regplot.png') 
+            plt.savefig('graph/4/regplot.png') 
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -122,7 +122,7 @@ def Draw(data,features,*xml):
             plt.xticks(rotation=90)
             x_axis = plt.gca().xaxis
             x_axis.set_tick_params(rotation=45)
-            plt.savefig('graph/violinplot.png') 
+            plt.savefig('graph/4/violinplot.png') 
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -131,7 +131,7 @@ def Draw(data,features,*xml):
             fig = plt.figure()
             sns.scatterplot(data=data, x=values['X'][0], y=y, size=values['X'][1], legend=False, sizes=(20, 2000))
             plt.xticks(rotation=90)
-            plt.savefig('graph/bubbleplot.png') 
+            plt.savefig('graph/4/bubbleplot.png') 
             pdf_pages.savefig(fig)
             plt.show()
             plt.close()
@@ -151,76 +151,77 @@ def Draw(data,features,*xml):
     # 遍歷所有欄位名稱的組合
     facetGrid =1
     # TODO: 測試待刪除
-    dropcol = ['產業名稱']
+    # dropcol = ['產業名稱']
 
     # 移除指定的元素
-    combinations_list = [combo for combo in combinations_list if not any(col in combo for col in dropcol)]
+    # combinations_list = [combo for combo in combinations_list if not any(col in combo for col in dropcol)]
     
     
     
     for combo in combinations_list:
 
         col_name, row_name, scatter_col = combo
-        # 動態生成新的欄位名稱，將原名稱後面添加 '_b'
-        col_name_b = col_name + '_b'
-        row_name_b = row_name + '_b'
-        # 根據類型,動態設定 col 和 row 參數
-        
-        # data[col_name_b] = pd.qcut(data[col_name].fillna(0), q=5, duplicates='drop')
-        # data[row_name_b] = pd.qcut(data[row_name].fillna(0), q=5, duplicates='drop')
-        
-        if pd.api.types.is_numeric_dtype(data[col_name]):
-            data[col_name_b] = pd.qcut(data[col_name].fillna(0), q=5, duplicates='drop')
-        else:
-            data[col_name_b] = pd.Categorical(data[col_name].fillna("missing"))
 
-        if pd.api.types.is_numeric_dtype(data[row_name]):
-            data[row_name_b] = pd.qcut(data[row_name].fillna(0), q=5, duplicates='drop')
-        else:
-            data[row_name_b] = pd.Categorical(data[row_name].fillna("missing"))
+        if(pd.api.types.is_numeric_dtype(data[scatter_col])):
+            # 動態生成新的欄位名稱，將原名稱後面添加 '_b'
+            col_name_b = col_name + '_b'
+            row_name_b = row_name + '_b'
+            # 根據類型,動態設定 col 和 row 參數
             
-        p = sns.FacetGrid(data, col=col_name_b, row=row_name_b, hue='證券代碼', margin_titles=True)
-        
-            # 自定義繪圖函數
-        def custom_scatter(*args, **kwargs):
-            ax = plt.gca()  # 獲取當前子圖的座標軸對象
-            ax.spines['bottom'].set_linewidth(3) # 設定底部線條寬度
-            ax.spines['left'].set_linewidth(3)   # 設定左側線條寬度
-            # 設定底部線條和左側線條的顏色為深黑色
-            ax.spines['bottom'].set_color('black')
-            ax.spines['left'].set_color('black')
+            # data[col_name_b] = pd.qcut(data[col_name].fillna(0), q=5, duplicates='drop')
+            # data[row_name_b] = pd.qcut(data[row_name].fillna(0), q=5, duplicates='drop')
+            
+            if pd.api.types.is_numeric_dtype(data[col_name]):
+                data[col_name_b] = pd.qcut(data[col_name].fillna(0), q=5, duplicates='drop')
+            else:
+                data[col_name_b] = pd.Categorical(data[col_name].fillna("missing"))
 
-            # 設定 Y 軸刻度標籤格式，以包含負號
-            ax.xaxis.set_major_formatter(ticker.FormatStrFormatter("%+.2f"))
-            ax.yaxis.set_major_formatter(ticker.FormatStrFormatter("%+.2f"))
-            plt.scatter(*args, **kwargs)
-            # 繪製散點圖或其他繪圖
-            # 根據 scatter_col 的資料型態，選擇不同的繪圖方式
-            # if pd.api.types.is_numeric_dtype(args[0]):
-            #     plt.scatter(*args, **kwargs)
-            # else:
-            #     x_data = args[1] if len(args) > 1 else None
-            #     y_data = args[0] if len(args) > 0 else None
-            #     plt.bar(x=x_data, height=y_data, **kwargs)
+            if pd.api.types.is_numeric_dtype(data[row_name]):
+                data[row_name_b] = pd.qcut(data[row_name].fillna(0), q=5, duplicates='drop')
+            else:
+                data[row_name_b] = pd.Categorical(data[row_name].fillna("missing"))
+                
+            p = sns.FacetGrid(data, col=col_name_b, row=row_name_b, hue='證券代碼', margin_titles=True)
+            
+                # 自定義繪圖函數
+            def custom_scatter(x,y,*args, **kwargs):
+                ax = plt.gca()  # 獲取當前子圖的座標軸對象
+                ax.spines['bottom'].set_linewidth(3) # 設定底部線條寬度
+                ax.spines['left'].set_linewidth(3)   # 設定左側線條寬度
+                # 設定底部線條和左側線條的顏色為深黑色
+                ax.spines['bottom'].set_color('black')
+                ax.spines['left'].set_color('black')
 
-        # 調整標題與圖的距離
-        plt.subplots_adjust(top=0.7)  # 調整標題與圖的距離，可以根據需要調整top值
-        #設定標題
-        p.fig.suptitle(f"在不同{col_name}(X) 和 {row_name}(Y)區間中，{scatter_col}(x)和股價變化率(y)的表現", fontsize=16)
-        p.map(custom_scatter, scatter_col, '區間股價變化率')
-     
-        for ax in p.axes.flat:
-            ax.spines['bottom'].set_linewidth(3)
-            ax.spines['left'].set_linewidth(3)
-            ax.spines['bottom'].set_color('black')
-            ax.spines['left'].set_color('black')
-        plt.show()
-        pdf_pages.savefig(p.fig)
-        p.fig.savefig(f'graph/facetGrid_{facetGrid}.png') 
+                # 設定 Y 軸刻度標籤格式，以包含負號
+                ax.xaxis.set_major_formatter(ticker.FormatStrFormatter("%+.2f"))
+                ax.yaxis.set_major_formatter(ticker.FormatStrFormatter("%+.2f"))
+                sns.regplot(x=x, y=y, *args, **kwargs)
+                # plt.scatter(*args, **kwargs)
+                # 繪製散點圖或其他繪圖
+                #根據 scatter_col 的資料型態，選擇不同的繪圖方式
+                # if pd.api.types.is_numeric_dtype(scatter_col):
+                #     plt.scatter(*args, **kwargs)
+                # else:
+                #     plt.bar(*args, **kwargs)
+
+            # 調整標題與圖的距離
+            plt.subplots_adjust(top=0.7)  # 調整標題與圖的距離，可以根據需要調整top值
+            #設定標題
+            p.fig.suptitle(f"在不同{col_name}(X) 和 {row_name}(Y)區間中，{scatter_col}(x)和股價變化率(y)的表現", fontsize=16)
+            p.map(custom_scatter, scatter_col, '區間股價變化率')
         
-        plt.close()
-        
-        facetGrid+=1
+            for ax in p.axes.flat:
+                ax.spines['bottom'].set_linewidth(3)
+                ax.spines['left'].set_linewidth(3)
+                ax.spines['bottom'].set_color('black')
+                ax.spines['left'].set_color('black')
+            plt.show()
+            pdf_pages.savefig(p.fig)
+            p.fig.savefig(f'graph/4/facetGrid_{facetGrid}.png') 
+            
+            plt.close()
+            
+            facetGrid+=1
 
     # 顯示圖形
     # plt.show()
