bd740fed761400cce90a3595b618ff203d41cfb1
Cynthia <cynthiaiii4@hotmail.com>
2023-11-23 11:48:35 +0800

    調整註釋

--- Utils/GetExtraX.py
+++ Utils/GetExtraX.py
@@ -10,20 +10,20 @@ def GetExtraX(data,xml):
     root = tree.getroot()
     
     for newX in root.findall('newX'):
-        name = newX.find('name').text  # 获取新列的名称
-        operands = []  # 用于存储操作数的列表
+        name = newX.find('name').text  # 獲取新列的名稱
+        operands = []  
 
-        # 获取操作数
+        
         for operand_tag in newX:
             if operand_tag.tag != 'name' and operand_tag.tag != 'Formula':
                 operands.append(data[operand_tag.text])
 
-        formula = newX.find('Formula').text  # 获取计算公式
+        formula = newX.find('Formula').text  # 獲取計算公式
 
-        # 构建动态的计算字典
+        # 根據運算元建立字典
         eval_dict = {f'operand{i}': operand for i, operand in enumerate(operands, 1)}
 
-        # 计算新列的值
+        # 計算新列的值
         result = eval(formula, eval_dict)
         data[name] = result
         
